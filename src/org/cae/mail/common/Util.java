4bd8539a14be12b46dbf3ba52af2d4954895d8c1
liuyang <liuyang@windows10.microdone.cn>
2017-08-01 03:26:44 +0800

    移除了XML相关工具类，并加入了读取文件文本工具fileReader类和查找JSON文件工具getJSONFile类

--- src/org/cae/mail/common/Util.java
+++ src/org/cae/mail/common/Util.java
@@ -1,7 +1,10 @@
 package org.cae.mail.common;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -10,40 +13,26 @@ import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
-import javax.xml.XMLConstants;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
-
 import org.apache.log4j.Logger;
-import org.dom4j.Document;
-import org.dom4j.io.SAXReader;
-import org.dom4j.io.SAXValidator;
-import org.dom4j.util.XMLErrorHandler;
-import org.xml.sax.SAXException;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Util {
 
-	private static SimpleDateFormat dateSdf=new SimpleDateFormat("yyyy-MM-dd");
-	private static SimpleDateFormat timeSdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	
-	public static String toJson(Object target){
-		ObjectMapper mapper=new ObjectMapper();
-		try{
+	private static SimpleDateFormat dateSdf = new SimpleDateFormat("yyyy-MM-dd");
+	private static SimpleDateFormat timeSdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+	public static String toJson(Object target) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
 			return mapper.writeValueAsString(target);
-		}catch(Exception ex){
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		return null;
 	}
-	
-	public static <T> T toObject(String json,Class<T> clazz){
-		ObjectMapper mapper=new ObjectMapper();
+
+	public static <T> T toObject(String json, Class<T> clazz) {
+		ObjectMapper mapper = new ObjectMapper();
 		try {
 			return mapper.readValue(json, clazz);
 		} catch (IOException e) {
@@ -51,130 +40,130 @@ public class Util {
 		}
 		return null;
 	}
-	
-	public static String date2String(Date date){
+
+	public static String date2String(Date date) {
 		return dateSdf.format(date);
 	}
-	
-	public static String time2String(Date date){
+
+	public static String time2String(Date date) {
 		return timeSdf.format(date);
 	}
 
-	public static String getNowDate(){
+	public static String getNowDate() {
 		return date2String(new Date());
 	}
-	
-	public static String getNowTime(){
+
+	public static String getNowTime() {
 		return time2String(new Date());
 	}
-	
-	public static String getBefore(long time){
-		return time2String(new Date(System.currentTimeMillis()-time));
+
+	public static String getBefore(long time) {
+		return time2String(new Date(System.currentTimeMillis() - time));
 	}
-	
-	public static String getCharId(){
+
+	public static String getCharId() {
 		return getCharId(new String(), 10);
 	}
-	
-	public static String getCharId(int size){
-		return getCharId(new String(),size);
+
+	public static String getCharId(int size) {
+		return getCharId(new String(), size);
 	}
-	
-	public static String getCharId(String pre,int size){
-		StringBuffer theResult=new StringBuffer();
+
+	public static String getCharId(String pre, int size) {
+		StringBuffer theResult = new StringBuffer();
 		theResult.append(pre);
 		String a = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		for(int i=0;i<size-pre.length();i++){
-			int rand =(int)(Math.random() * a.length());
+		for (int i = 0; i < size - pre.length(); i++) {
+			int rand = (int) (Math.random() * a.length());
 			theResult.append(a.charAt(rand));
 		}
 		return theResult.toString();
 	}
-	
-	public static short getRandom(int randomRange){
-		Random random=new Random();
+
+	public static short getRandom(int randomRange) {
+		Random random = new Random();
 		return (short) random.nextInt(randomRange);
 	}
-	
-	public static boolean isNotNull(Object object){
-		boolean result=false;
-		if(object==null)
+
+	public static boolean isNotNull(Object object) {
+		boolean result = false;
+		if (object == null)
 			return result;
-		if(object instanceof String){
-			String temp=(String) object;
-			if(temp!=null&&!temp.equals(""))
-				result=true;
+		if (object instanceof String) {
+			String temp = (String) object;
+			if (temp != null && !temp.equals(""))
+				result = true;
 			else
-				result=false;
-		}
-		else if(object instanceof List){
-			List list=(List) object;
-			if(list.size()>0)
-				result=true;
+				result = false;
+		} else if (object instanceof List) {
+			List list = (List) object;
+			if (list.size() > 0)
+				result = true;
 			else
-				result=false;
+				result = false;
 		}
 		return result;
 	}
-	
-	public static void logStackTrace(Logger logger,StackTraceElement[] stackTrace){
-		String stackInfo="";
-		for(StackTraceElement element:stackTrace){
-			stackInfo+=element+"\n";
+
+	public static void logStackTrace(Logger logger, StackTraceElement[] stackTrace) {
+		String stackInfo = "";
+		for (StackTraceElement element : stackTrace) {
+			stackInfo += element + "\n";
 		}
 		logger.error(stackInfo);
 	}
-	
-	public static String md5(String str){
-		try{
-			MessageDigest md=MessageDigest.getInstance("MD5");
+
+	public static String md5(String str) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
 			md.update(str.getBytes());
-			return new BigInteger(1,md.digest()).toString(16);
-		}catch(Exception ex){
+			return new BigInteger(1, md.digest()).toString(16);
+		} catch (Exception ex) {
 			return null;
 		}
 	}
+
 	/**
 	 * 
-	 * @param Xml文件的路径
-	 * @param Xsd文件的路径
-	 * @return 返回校验成功的标志
+	 * @param jsonDir
+	 *            JSON文件所在的目录
+	 * @return JSON文件数组
 	 */
-	public static boolean validateXML(String xmlFilePath,String xsdFilePath) { 
-			  long startTime = System.currentTimeMillis(); 
-		        try {
-		            SchemaFactory factory = 
-		                    SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-		            Schema schema = factory.newSchema(new File(xsdFilePath));
-		            Validator validator = schema.newValidator();
-		            validator.validate(new StreamSource(new File(xmlFilePath)));
-		        } catch (IOException | SAXException e) {
-		            System.out.println("Exception: "+e.getMessage()); 
-		            return false;
-		        }
-		        return true;
-		    }
-	/**
-	 * 
-	 * @param xmlDir XML文件所在文件夹的路径
-	 * @return	XML文件数组
-	 */
-	public static File[] getXMLFile(String xmlDir){
-		File file=new File(xmlDir);
-		if(!file.exists()){
+	public static File[] getJsonFile(String jsonDir) {
+		File file = new File(jsonDir);
+		if (!file.exists()) {
 			file.mkdir();
 		}
-		File[] xmlFiles=new File(xmlDir).listFiles(new FileFilter() {
+		File[] jsonFiles = new File(jsonDir).listFiles(new FileFilter() {
 			@Override
 			public boolean accept(File pathname) {
 				String filename = pathname.getName().toLowerCase();
-				if(filename.endsWith(".xml")){
+				if (filename.endsWith(".json")) {
 					return true;
-				}else{
+				} else {
 					return false;
 				}
 			}
 		});
-		return xmlFiles;
+		return jsonFiles;
+	}
+
+	/**
+	 * 
+	 * @param file
+	 *            文件
+	 * @return 文件文本
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
+	public static String fileReader(File file) throws IOException, FileNotFoundException {
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+		StringBuffer buffer = new StringBuffer();
+		String s = null;
+		while ((s = bufferedReader.readLine()) != null) {
+			buffer.append(s);
+		}
+		bufferedReader.close();
+		return buffer.toString();
 	}
 }
