fffe4816edc795f0c2c695b6028ee8997849220b
liuyang <liuyang@windows10.microdone.cn>
2017-08-02 16:29:41 +0800

    增加用于判断枚举常量是否存在的方法

--- src/org/cae/mail/entity/MailType.java
+++ src/org/cae/mail/entity/MailType.java
@@ -2,4 +2,12 @@ package org.cae.mail.entity;
 
 public enum MailType {
 	ANDROID_ADVICE,JAVA_ADVICE;
+	public static boolean contains(String type){    
+        for(MailType mailType : MailType.values()){    
+            if(mailType.name().equals(type)){    
+                return true;    
+            }    
+        }    
+        return false;    
+    }    
 }
